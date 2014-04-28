d03a328c63201b368b6a2b80e57aad35df6ff64e
DaeGlo <daeglo@daeglo.com>
2014-04-27 18:29:57 -0600

    Merge Item -> Category: add item functionality to Category.

--- app/Model/Category.js
+++ app/Model/Category.js
@@ -1,35 +1,92 @@
 /* jshint es3:true */
 /* globals define:false */
-
-define(['lodash'], function (_) {
+define(['Model/Period', 'lodash'], function (Period, _) {
     'use strict';
 
     /**
      * A collection of budget items which acts like a budget item
-     * @param a_name {string} a description of the category
-     * @param an_items {array<Item>} a collection of items
+     * @param {String} a_name a description of the category
+     * @param {Number} an_amount The value of this category over a_period
+     * @param {Period} a_period The period in which this category cycles
+     * @param {Array<Item>} sub_items A collection of items providing more detail
      */
-    function Category(a_name, an_items) {
-        this.name = a_name;
-        this.items = an_items || [];
-
-        this.per = function per(a_period) {
-            if (this.items.length === 0) {
+    function Category(a_name, an_amount, a_period, sub_items) {
+        var myAmount = an_amount || 0,
+            myItems = [],
+            subTotal = function () {
+                if (myItems.length !== 0) {
+                    return _.map(myItems, function (item) {
+                        return item.per(a_period);
+                    }).
+                    reduce(function (sum, item) {
+                        return sum + item;
+                    });
+                }
                 return 0;
+            },
+            getAmount = function () {
+                return myAmount;
+            },
+            setAmount = function (an_amount) {
+                var subt = subTotal();
+                if (an_amount < subt) {
+                    throw 'Must be greater than the sum of it\'s parts';
+                }
+                myAmount = an_amount - subt;
+            };
+
+        this.name = a_name;
+        this.period = a_period || Period.PerMonth;
+
+        Object.defineProperties(this, {
+            amount: {
+                enumerable: true,
+                get: getAmount,
+                set: setAmount
+            },
+            items: {
+                enumerable: true,
+                get: function () {
+                    return myItems;
+                }
             }
-            return _.map(this.items, function (item) {
-                return item.per(a_period);
-            }).
-            reduce(function (sum, item) {
-                return sum + item;
-            });
+        });
+
+        this.per =
+        /**
+         * Amortizes the value of this item over a_period
+         * @param {Period} a_period The period to evaluate
+         * @returns {number} The amortized value
+         */
+        function per(a_period) {
+            var value = myAmount / this.period.multiplier * a_period.multiplier;
+            value += subTotal();
+            return value;
         };
-        this.addBudgetItem = function addBudgetItem(an_item) {
-            this.items.push(an_item);
+
+        this.addBudgetItem =
+        /**
+         * Adds an_item to this category
+         * @param {Item} an_item The item to add
+         */
+        function addBudgetItem(an_item) {
+            myAmount -= an_item.per(this.period);
+            myItems.push(an_item);
         };
-        this.removeBudgetItem = function removeBudgetItem(an_item) {
-            _.remove(this.items, an_item);
+
+        this.removeBudgetItem =
+        /**
+         * Removes an_item from this category
+         * @param {Item} an_item The item to remove
+         */
+        function removeBudgetItem(an_item) {
+            _.remove(myItems, an_item);
         };
+
+        sub_items = sub_items || [];
+        for (var i = 0; i < sub_items.length; i++) {
+            this.addBudgetItem(sub_items[i]);
+        }
     }
 
     return Category;
